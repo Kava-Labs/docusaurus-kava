ce409906e2353fd96514bffb8e07d899aab87776
Sam Hadi <worker@Sams-MacBook-Pro.local>
2022-02-25 13:22:39 -0800

    rm imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,4 @@
-import React, { useEffect } from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
-
+import { useEffect } from 'react';
 
 
 export default function Home(): JSX.Element {
