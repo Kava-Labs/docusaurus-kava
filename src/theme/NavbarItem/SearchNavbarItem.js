36aed7a367ebc4d12f4bc907d9682578344a77b9
adamrturman <adamrturman@gmail.com>
2022-03-01 11:40:50 -0600

    clean up style copypasta

--- src/theme/NavbarItem/SearchNavbarItem.js
+++ src/theme/NavbarItem/SearchNavbarItem.js
@@ -5,7 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  */
 import React from 'react';
-import SearchBar from '@theme/SearchBar';
 export default function SearchNavbarItem({ mobile }) {
   if (mobile) {
     return null;
