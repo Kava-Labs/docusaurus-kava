2005efff153e990cf8d421c3dead1e8641448b2b
adamrturman <adamrturman@gmail.com>
2022-03-02 15:01:34 -0600

    extend hover fill color to arrows

--- src/components/NextArrow/index.tsx
+++ src/components/NextArrow/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from './styles.module.css';
 
 export default function NextArrow() {
-  const fillColor = 'var(--ifm-color-primary)';
+  // const fillColor = 'var(--ifm-color-primary)';
 
   return (
     <div className={classes.arrowWrapper}>
@@ -15,7 +15,7 @@ export default function NextArrow() {
       >
         <path
           d="M6.11959e-07 9L12.17 9L6.58 14.59L8 16L16 8L8 -6.99382e-07L6.59 1.41L12.17 7L7.86805e-07 7L6.11959e-07 9Z"
-          fill={fillColor}
+          fill="currentColor"
         />
       </svg>
     </div>
--- src/components/PreviousArrow/index.tsx
+++ src/components/PreviousArrow/index.tsx
@@ -13,7 +13,7 @@ export default function PreviousArrow() {
       >
         <path
           d="M16 7H3.83L9.42 1.41L8 0L0 8L8 16L9.41 14.59L3.83 9H16V7Z"
-          fill="#FF564F"
+          fill="currentColor"
         />
       </svg>
     </div>
