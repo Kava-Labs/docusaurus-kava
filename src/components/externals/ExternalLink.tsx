5719ffd1bfbce6885c9a87bb5b33cb3ee25f9cc1
Sam Hadi <worker@Sams-MBP.attlocal.net>
2022-03-08 15:08:35 -0600

    trying something

--- src/components/externals/ExternalLink.tsx
+++ src/components/externals/ExternalLink.tsx
@@ -2,16 +2,13 @@ import { useEffect } from 'react';
 
 export default function ExternalLink({ url }: { url: string }) {
   useEffect(() => {
-    // create an a tag and simulate a click
+    // create an a element and simulate a click
     // this works across all browsers
     const a = document.createElement('a');
-    // display none
     a.setAttribute('style', 'display: none;');
-    // add the url
+    a.setAttribute('rel', 'noopener');
+    a.setAttribute('target', 'about:blank');
     a.setAttribute('href', url);
-    // ensure it opens in a new tab
-    a.setAttribute('target', '_blank');
-    //  click on the a tag
     a.click();
     // go back to previous page
     window.history.back();
